cebebd838b8d8849da455714b49030223721e75f
Temidayo <temidayoakin.pro@gmail.com>
2025-08-24 06:21:22 +0100

    Add files via upload

---
+++ scripts/cummulative_analysis.sql
@@ -0,0 +1,52 @@
+/*
+===============================================================================
+Cumulative Analysis
+===============================================================================
+Purpose:
+    - To calculate running totals or moving averages for key metrics.
+    - To track performance over time cumulatively.
+    - Useful for growth analysis or identifying long-term trends.
+
+SQL Functions Used:
+    - Window Functions: SUM() OVER(), AVG() OVER()
+===============================================================================
+*/
+
+-- Calculate the total sales per month 
+-- and the running total of sales over time 
+
+
+SELECT
+	order_month,
+	total_sales,
+	-- window function
+	SUM(total_sales) OVER(PARTITION BY DATEPART(year,order_month) ORDER BY order_month) AS running_total_sales,
+	AVG(avg_price) OVER(PARTITION BY DATEPART(year,order_month) ORDER BY order_month) AS moving_average_price
+FROM(   
+		SELECT
+		DATETRUNC(month, order_date) AS order_month,
+		SUM(sales_amount) AS total_sales,
+		AVG(price) AS avg_price
+		FROM gold.fact_sales
+		WHERE order_date IS NOT NULL
+		GROUP BY DATETRUNC(month, order_date)
+)t
+
+-- Calculate the total sales per year 
+-- and the running total of sales over time 
+
+SELECT
+	order_year,
+	total_sales,
+	-- window function
+	SUM(total_sales) OVER(ORDER BY order_year) AS running_total_sales,
+	AVG(avg_price) OVER(ORDER BY order_year) AS moving_average_price
+FROM(   
+		SELECT
+		DATETRUNC(year, order_date) AS order_year,
+		SUM(sales_amount) AS total_sales,
+		AVG(price) AS avg_price
+		FROM gold.fact_sales
+		WHERE order_date IS NOT NULL
+		GROUP BY DATETRUNC(year, order_date)
+)t
